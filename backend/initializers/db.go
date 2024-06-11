704081911660ab8bfddf30342c8c01f7d063bfd7
mirzakarabulut <mirzakara1234@gmail.com>
2024-06-11 17:26:47 +0300

    chore: Update database connection to use PostgreSQL with GORM

--- backend/initializers/db.go
+++ backend/initializers/db.go
@@ -1,39 +1,23 @@
 package initializers
 
 import (
-	"context"
+	"fmt"
 	"log"
 	"os"
-	"strings"
 
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func ConnectDB() *mongo.Client {
-    // Get environment variables
-    databaseURI := os.Getenv("DATABASE")
-    databasePassword := os.Getenv("DATABASE_PASSWORD")
+var DB *gorm.DB
 
-    // Replace <password> placeholder with actual password
-    databaseURI = strings.Replace(databaseURI, "<password>", databasePassword, 1)
-
-    // Set client options
-    clientOptions := options.Client().ApplyURI(databaseURI)
-
-    // Connect to MongoDB
-    client, err := mongo.Connect(context.TODO(), clientOptions)
-
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    // Check the connection
-    err = client.Ping(context.TODO(), nil)
-
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    return client
+func Connect() {
+	var err error
+	dsn := os.Getenv("DB_URL")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Couldn't connect to the DB")
+	} else {
+		fmt.Println("Connected to the DB")
+	}
 }
