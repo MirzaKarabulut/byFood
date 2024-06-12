8a1bae1d864a184afda6af266890d65b2a43064e
mirzakarabulut <mirzakara1234@gmail.com>
2024-06-12 11:52:15 +0300

    chore: Add migration file for initializing database and auto-migrating Book model

---
+++ backend/migrate/migrate.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"backend/initializers"
+	"backend/models"
+)
+
+func init() {
+	initializers.LoadEnvVars()
+	initializers.Connect()
+}
+
+func main() {
+	 initializers.DB.AutoMigrate(&models.Book{})
+}
