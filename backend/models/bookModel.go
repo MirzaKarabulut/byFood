296c54a7eaf8eb4dff80a7a227106b99b6a75164
mirzakarabulut <mirzakara1234@gmail.com>
2024-06-15 23:18:14 +0300

    Refactor bookModel.go to remove unused ID field and add missing newline at end of file

--- backend/models/bookModel.go
+++ backend/models/bookModel.go
@@ -6,7 +6,6 @@ import (
 
 type Book struct {
 	gorm.Model
-	ID				 		uint               `json:"id" gorm:"primaryKey"`
 	Title       	*string             `json:"title"`
 	Author     	 	*string             `json:"author"`
 	ReleaseDate 	*string             `json:"release_date"`
