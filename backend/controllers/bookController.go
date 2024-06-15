8f5fbbcbda1804e11df15f4885f84ab21e6aa053
mirzakarabulut <mirzakara1234@gmail.com>
2024-06-15 19:48:24 +0300

    chore: Refactor bookController.go to use structured request body in CreateBook and UpdateBook functions

--- backend/controllers/bookController.go
+++ backend/controllers/bookController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"backend/initializers"
 	"backend/models"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,13 +15,6 @@ func GetAllBooks(c *gin.Context)  {
 	c.JSON(200, books)
 }
 
-func CreateBook(c *gin.Context) {
-	var book models.Book
-	c.BindJSON(&book)
-	initializers.DB.Create(&book)
-	c.JSON(200, book)
-}
-
 func GetBookByID(c *gin.Context) {
 	id := c.Param("id")
 	var book models.Book
@@ -28,19 +22,45 @@ func GetBookByID(c *gin.Context) {
 	c.JSON(200, book)
 }
 
+func CreateBook(c *gin.Context) {
+	var body struct {
+			Title    string
+			Author string
+			Description string
+			ReleaseDate string
+	}
+	c.Bind(&body)
+
+	book := models.Book{Title: &body.Title,Author: &body.Author, Description: &body.Description, ReleaseDate: &body.ReleaseDate}
+	result := initializers.DB.Create(&book)
+
+	if result.Error != nil {
+		fmt.Println(result.Error)
+		c.Status(400)
+		return
+	}
+
+	c.JSON(200, book)
+}
+
+
 func UpdateBook(c *gin.Context) {
 	id := c.Param("id")
+	var body struct {
+			Title    string
+			Author string
+			Description string
+			ReleaseDate string
+	}
+	c.Bind(&body)
 	var book models.Book
 	initializers.DB.First(&book, id)
-	c.BindJSON(&book)
-	initializers.DB.Save(&book)
+	initializers.DB.Model(&book).Updates(models.Book{Title: &body.Title, Author: &body.Author, Description: &body.Description, ReleaseDate: &body.ReleaseDate})
 	c.JSON(200, book)
 }
 
 func DeleteBook(c *gin.Context) {
 	id := c.Param("id")
-	var book models.Book
-	initializers.DB.First(&book, id)
-	initializers.DB.Delete(&book)
-	c.JSON(200, book)
+	initializers.DB.Delete(&models.Book{}, id)
+	c.JSON(200, id)
 }
