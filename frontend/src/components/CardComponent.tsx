9394e8abd5b89007819b4528df1d3228195dd75d
mirzakarabulut <mirzakara1234@gmail.com>
2024-06-16 14:42:10 +0300

    Refactor CardComponent to use div elements instead of h1, h2, and p for better accessibility

--- frontend/src/components/CardComponent.tsx
+++ frontend/src/components/CardComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 interface Card {
   id: number;
@@ -8,14 +8,14 @@ interface Card {
   releaseDate: string;
 }
 
-const CardComponent: React.FC<{card: Card} > = ({ card}) => {
+const CardComponent: React.FC<{ card: Card }> = ({ card }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg p-2m mb-2 hover:bg-gray-100">
-      <h1 className="text-sm text-gray-600">{card.id}</h1>
-      <h2 className="text-lg font-semibold text-gray-800">{card.title}</h2>
-      <h3 className="text-lg font-semibold text-gray-800">{card.author}</h3>
-      <p className="text-md text-gray-700">{card.description}</p>
-      <p className="text-md text-gray-700">{card.releaseDate}</p>
+    <div className="bg-white shadow-lg rounded-lg p-2 mb-2 hover:bg-gray-100">
+      <div className="text-sm text-gray-600">{card.id}</div>
+      <div className="text-lg font-semibold text-gray-800">{card.title}</div>
+      <div className="text-md text-gray-700">{card.author}</div>
+      <div className="text-md text-gray-700">{card.description}</div>
+      <div className="text-md text-gray-700">{card.releaseDate}</div>
     </div>
   );
 }
