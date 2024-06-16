24a5d3cefdfeb70b6820d14646ffd3f58455c0f2
mirzakarabulut <mirzakara1234@gmail.com>
2024-06-16 13:13:09 +0300

    feat: Add author and release date fields to CardComponent

--- frontend/src/components/CardComponent.tsx
+++ frontend/src/components/CardComponent.tsx
@@ -3,7 +3,9 @@ import React from "react";
 interface Card {
   id: number;
   title: string;
+  author: string;
   description: string;
+  releaseDate: string;
 }
 
 const CardComponent: React.FC<{card: Card} > = ({ card}) => {
@@ -11,7 +13,9 @@ const CardComponent: React.FC<{card: Card} > = ({ card}) => {
     <div className="bg-white shadow-lg rounded-lg p-2m mb-2 hover:bg-gray-100">
       <h1 className="text-sm text-gray-600">{card.id}</h1>
       <h2 className="text-lg font-semibold text-gray-800">{card.title}</h2>
+      <h3 className="text-lg font-semibold text-gray-800">{card.author}</h3>
       <p className="text-md text-gray-700">{card.description}</p>
+      <p className="text-md text-gray-700">{card.releaseDate}</p>
     </div>
   );
 }
