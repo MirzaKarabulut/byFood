d68c8ae6458f00c07ba28ce61415c18ecf1ff394
mirzakarabulut <mirzakara1234@gmail.com>
2024-06-17 22:32:07 +0300

    feat: Update bookController.go to improve code readability and add logging

--- backend/controllers/bookController.go
+++ backend/controllers/bookController.go
@@ -3,51 +3,92 @@ package controllers
 import (
 	"backend/initializers"
 	"backend/models"
-	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
-
-func GetAllBooks(c *gin.Context)  {
+func GetAllBooks(c *gin.Context) {
+	// Log event
+	LogEvent(c, "Get All Books", "Book", "Success", "Retrieved all books")
+	// Log the request
+	LogRequest(c, "200")
+	// Retrieve all books from the database
 	var books []models.Book
- initializers.DB.Find(&books)
+	initializers.DB.Find(&books)
+
+	// Return the books as JSON response
 	c.JSON(200, books)
 }
 
 func GetBookByID(c *gin.Context) {
+	// Log event
+	LogEvent(c, "Get Book By ID", "Book", "Success", "Retrieved book by ID")
+
+	// Log the request
+	LogRequest(c, "200")
+
 	id := c.Param("id")
 	var book models.Book
 	initializers.DB.First(&book, id)
+
 	c.JSON(200, book)
 }
 
 func CreateBook(c *gin.Context) {
+	// Parse the request body
 	var body struct {
-			Title    string
-			Author string
-			Description string
-			ReleaseDate string
+		Title       string `json:"Title" validate:"required"`
+		Author      string `json:"Author" validate:"required"`
+		Description string `json:"Description" validate:"required"`
+		ReleaseDate string `json:"ReleaseDate" validate:"required" validate:"datetime=2006-01-02"`
 	}
-	c.Bind(&body)
-	fmt.Println(body)
+	err := c.Bind(&body)
+	if err != nil {
+		return
+	}
 
-	book := models.Book{Title: &body.Title,Author: &body.Author, Description: &body.Description, ReleaseDate: &body.ReleaseDate}
-	initializers.DB.Create(&book)
+	validate := validator.New()
+
+	err = validate.Struct(body)
+	if err != nil {
+		LogEvent(c, "Create Book", "Book", "Validation", err.Error())
+		LogRequest(c, "400")
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	tx := initializers.DB.Begin()
+
+	book := models.Book{Title: &body.Title, Author: &body.Author, Description: &body.Description, ReleaseDate: &body.ReleaseDate}
+	result := tx.Create(&book)
+	if result.Error != nil {
+		LogEvent(c, "Create Book", "Book", "Database", result.Error.Error())
+		LogRequest(c, "400")
+		tx.Rollback()
+		c.JSON(400, result.Error)
+		return
+	}
+
+	tx.Commit()
+	LogEvent(c, "Create Book", "Book", "Success", "Book created successfully")
+	LogRequest(c, "200")
 
 	c.JSON(200, book)
 }
 
-
 func UpdateBook(c *gin.Context) {
 	id := c.Param("id")
 	var body struct {
-			Title    string
-			Author string
-			Description string
-			ReleaseDate string
+		Title       string
+		Author      string
+		Description string
+		ReleaseDate string
 	}
-	c.Bind(&body)
+	err := c.Bind(&body)
+	if err != nil {
+		return
+	}
 	var book models.Book
 	initializers.DB.First(&book, id)
 	initializers.DB.Model(&book).Updates(models.Book{Title: &body.Title, Author: &body.Author, Description: &body.Description, ReleaseDate: &body.ReleaseDate})
@@ -55,6 +96,12 @@ func UpdateBook(c *gin.Context) {
 }
 
 func DeleteBook(c *gin.Context) {
+	// Log event
+	LogEvent(c, "Delete Book", "Book", "Success", "Book deleted successfully")
+
+	// Log the request
+	LogRequest(c, "200")
+
 	id := c.Param("id")
 	initializers.DB.Delete(&models.Book{}, id)
 	c.JSON(200, id)
