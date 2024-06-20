e0616c47ae0f939addb2145710692cecc21f0944
mirzakarabulut <mirzakara1234@gmail.com>
2024-06-20 20:57:43 +0300

    feat: Refactor logController.go for improved logging functionality

--- backend/controllers/logController.go
+++ backend/controllers/logController.go
@@ -8,24 +8,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// LogRequest logs details of a request to the database
 func LogRequest(c *gin.Context, responseStatus string) {
 	logBody := models.RequestLog{
 		UserAgent: c.Request.UserAgent(),
 		SessionID: c.Request.Header.Get("SessionID"),
 		DateTime:  time.Now().Format("2006-01-02 15:04:05"),
-		Request:   c.Request.Method + "" + c.Request.URL.Path,
-		Response: responseStatus,
+		Request:   c.Request.Method + " " + c.Request.URL.Path,
+		Response:  responseStatus,
 	}
 	tx := initializers.DB.Begin()
 	result := tx.Create(&logBody)
 	if result.Error != nil {
 		tx.Rollback()
-		c.JSON(400, result.Error)
+		ErrorHandler(c, result.Error)
 		return
 	}
 	tx.Commit()
 }
 
+// LogEvent logs an event to the database
 func LogEvent(c *gin.Context, eventName string, source string, tags string, description string) {
 	logBody := models.EventLog{
 		EventName:   eventName,
@@ -40,7 +42,7 @@ func LogEvent(c *gin.Context, eventName string, source string, tags string, desc
 	result := tx.Create(&logBody)
 	if result.Error != nil {
 		tx.Rollback()
-		c.JSON(400, result.Error)
+		ErrorHandler(c, result.Error)
 		return
 	}
 	tx.Commit()
