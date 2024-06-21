412d9ccf74b99c10acc4580e577eb1e38e4c4973
mirzakarabulut <mirzakara1234@gmail.com>
2024-06-21 16:49:48 +0300

    Refactor ErrorHandler in errorController.go for improved error handling and response formatting

--- backend/controllers/errorController.go
+++ backend/controllers/errorController.go
@@ -1,12 +1,15 @@
 package controllers
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 // ErrorHandler handles errors and sends appropriate responses
-func ErrorHandler(c *gin.Context, err error) {
-	c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+func ErrorHandler(c *gin.Context, statusCode int, err error, message string, detail string) {
+    c.JSON(statusCode, gin.H{
+        "error":   err.Error(),
+        "message": message,
+        "detail":  detail,
+    })
+    c.Abort()
 }
